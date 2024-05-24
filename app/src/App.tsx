0aef2503ca5974cb5094070509dcd7df27bc8a2e
Goodacres <jack.goodacre2@hotmail.co.uk>
2024-05-24 21:11:39 +0200

    Fix CSS + header

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,19 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Providers } from "./providers";
+import Header from "@/lib/components/Header";
+import Home from "@/pages/Home";
 
 function App() {
   return (
     <>
-      <Providers>
-        <p>Hello</p>
-      </Providers>
+      <Router>
+        <Providers>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Providers>
+      </Router>
     </>
   );
 }
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), nodePolyfills()],
+  resolve: {
+    alias: {
+      "@/": path.resolve(__dirname, "./src"),
+      "@/lib": path.resolve(__dirname, "./src/lib"),
+      "@/components": path.resolve(__dirname, "./src/lib/components"),
+      "@/pages": path.resolve(__dirname, "./src/pages"),
+    },
+  },
+});
